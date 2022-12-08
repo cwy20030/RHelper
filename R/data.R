fc91ba60b0d76c8771650be6f4b7d5366c85a1f6
Willie (Chun) Yao <35785225+cwy20030@users.noreply.github.com>
2022-12-08 13:52:18 -0500

    2022-12-08

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Daily Forecast
+#'
+#' A simulated data for sleep study
+#'
+#' @usage data(NCSM_205_1994_Modified_K)
+#'
+#' @format A simulated data with 100 rows and 5 variables:
+#' \describe{
+#' \item{\code{Age}}{Age at the recuitment}
+#' \item{\code{Sex}}{Participant's biological sex}
+#' \item{\code{K10}}{Psychological distress scale}
+#' \item{\code{Insomnia}}{Insomnia symptom status}
+#' \item{\code{Apnea}}{Apnea symptom status}
+#' }
+"NCSM_205_1994_Modified_K"
+
+
+
+
