3ae4962830e330f96eb3deb4beb414dc9373ac51
Willie (Chun) Yao <35785225+cwy20030@users.noreply.github.com>
2024-08-21 16:19:40 -0400

    Update DirClass.R

--- R/DirClass.R
+++ R/DirClass.R
@@ -7,26 +7,26 @@
 #
 
 DirClass = function(Dir){
-
-
+  
+  
   # Check Pre-requisite --------
   for (x in c("utils", "tools"))
     if (!requireNamespace(x, quietly = T)) {
       install.packages(x)
       requireNamespace(x, quietly = T)
     }
-
+  
   # Check directory existance status
- if (!dir.exists(Dir)) stop(paste0(Dir," cannot be found. Please, check the spelling.") )
-
+  if (!dir.exists(Dir) & !file.exists(Dir)) stop(paste0(Dir," cannot be found. Please, check the spelling.") )
+  
   # If it is a directory
   if (file.info(Dir)$isdir) class(Dir) = "Folder"
-
+  
   # If it is a file
   if (!file.info(Dir)$isdir) class(Dir) = tools::file_ext(Dir)
-
-
-
+  
+  
+  
   return(Dir)
-
+  
 }
